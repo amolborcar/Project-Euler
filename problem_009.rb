e64aa9a657a6600a77b265d271862c62f2d52d48
Amol Borcar <aborcar@gmail.com>
2014-10-02 15:12:04 -0700

    small modification to problem 9.

--- problem_009.rb
+++ problem_009.rb
@@ -6,15 +6,12 @@ end
 
 def triplet
   a = 1
-  b = 1
   while a < 1000
-    a.upto(1000) do |x|
-      b = x
+    a.upto(1000) do |b|
       c = Math.sqrt(a**2 + b**2).prettify
       if c.is_a?(Integer) && a+b+c == 1000
         return a*b*c if a+b+c == 1000
       end
-      p c
     end
     a += 1
   end
