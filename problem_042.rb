7574d3a240a630d0b54c5abd5fa7e724dd3002cc
Amol Borcar <aborcar@gmail.com>
2014-10-10 14:33:57 -0700

    Solution to problem 42.
    This solution can probably be optimized.

---
+++ problem_042.rb
@@ -0,0 +1,55 @@
+def coded_triangle
+  # Create words array
+  f = File.open("./p042_words.txt", "r")
+  words = []
+  f.each_line do |line|
+    words = line.split(",").to_a
+  end
+  words.map! { |word| word.gsub!(/\W+/, '') }
+
+  # create first 100 triangle values
+  triangles = []
+  i = 1
+  while i < 100 do
+    triangles << (0.5)*(i*(i+1))
+    i += 1
+  end
+
+  # Check each word to see if it's a coded triangle word
+  words_counter = 0
+  # translation = {
+  #   "A" => 1,
+  #   "B" => 1,
+  #   "C" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+  #   "A" => 1,
+
+  # }
+  words.each do |word|
+    counter = 0
+    word.each_byte do |letter|
+      counter += (letter - 64)
+    end
+    if triangles.include?(counter)
+      words_counter += 1
+    end
+  end
+
+  return words_counter
+
+end
+
+p coded_triangle
